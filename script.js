08b0da5487d7f42ae9cab925693c839887a2dc87
vigneshsuresh56 <vigneshsureshnpr@gmail.com>
2026-04-13 16:28:44 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Variables
+let name = "Vignesh";
+const age = 20;
+
+// 2. Function
+function greet() {
+    console.log("Hello " + name);
+}
+
+// 3. Condition
+if (age > 18) {
+    console.log("Adult");
+} else {
+    console.log("Minor");
+}
+
+// 4. Loop
+for (let i = 0; i < 3; i++) {
+    console.log("Count: " + i);
+}
+
+// 5. Function call
+greet();
