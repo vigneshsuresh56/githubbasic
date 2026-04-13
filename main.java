a9305a239ceeaad473bfc06a91bfbae2fa8ac6a7
Vignesh <vigneshsureshnpr@gmail.com>
2026-04-13 18:06:44 +0530

    add the new file

---
+++ main.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Variables
+        int a = 10;
+        int b = 5;
+        int sum;
+
+        // Addition
+        sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Condition
+        if (a > b) {
+            System.out.println("A is greater than B");
+        } else {
+            System.out.println("B is greater");
+        }
+
+        // Loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
